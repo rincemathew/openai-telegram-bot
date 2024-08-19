642d3df9f68a06c98f5cfbc188f440d5bfd77e88
Rince Mathew <rincemathewmanattu@gmail.com>
2024-08-19 21:45:53 +0530

    telegram bot create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
 
 dotenv.config()
 
@@ -7,10 +8,24 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+const token = process.env.TELEGRAM_API;
 
-app.get('/',(req,res)=>{
-    res.send('heelllooo')
-})
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+  
+    if(msg.text === "/start") {
+        bot.sendMessage(chatId, 'Welcome to Chatbot powered by OPENAI, Ask anything you want...');
+    }
+    else if(msg.text) {
+        bot.sendMessage(chatId, 'Received your message');
+    } else {
+        bot.sendMessage(chatId, 'we only support text right now');
+    }
+    
+  });
 
 app.listen(PORT,()=>{
     console.log(`server started in port ${PORT}`)
