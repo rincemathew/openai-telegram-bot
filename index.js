ca3edcad18a2c523d528cf4af28ffa7f02630cc2
Rince Mathew <rincemathewmanattu@gmail.com>
2024-08-20 14:56:01 +0530

    open api configuration

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import TelegramBot from 'node-telegram-bot-api';
+import express from "express";
+import dotenv from "dotenv";
+import TelegramBot from "node-telegram-bot-api";
+import OpenAI from "openai";
 
-dotenv.config()
 
-const app = express()
+dotenv.config();
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 const token = process.env.TELEGRAM_API;
+const bot = new TelegramBot(token, { polling: true });
 
-const bot = new TelegramBot(token, {polling: true});
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-  
-    if(msg.text === "/start") {
-        bot.sendMessage(chatId, 'Welcome to Chatbot powered by OPENAI, Ask anything you want...');
-    }
-    else if(msg.text) {
-        bot.sendMessage(chatId, 'Received your message');
-    } else {
-        bot.sendMessage(chatId, 'we only support text right now');
-    }
-    
-  });
+  if (msg.text === "/start") {
+    bot.sendMessage(
+      chatId,
+      "Welcome to Chatbot powered by OPENAI, Ask anything you want..."
+    );
+  } else if (msg.text) {
+    const openCall = await openAICall(chatId, msg.text);
+    bot.sendMessage(chatId, openCall);
+  } else {
+    bot.sendMessage(chatId, "we only support text right now");
+  }
+});
 
-app.listen(PORT,()=>{
-    console.log(`server started in port ${PORT}`)
-})
+async function openAICall(chatId, text) {
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: text }],
+      model: "gpt-3.5-turbo",
+    });
+
+    return chatCompletion.choices[0].message.content;
+  } catch (error) {
+    console.log(error);
+    return "There was an error porcessing your request Please try again";
+  }
+}
+
+app.listen(PORT, () => {
+  console.log(`server started in port ${PORT}`);
+});
